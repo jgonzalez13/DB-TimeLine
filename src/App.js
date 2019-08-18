abe5a135855180ced7f56286632539bc15e605b7
Emmanuel Gonzalez <jgonzalez13@ucol.mx>
2019-08-18 18:47:24 -0500

    alternate mode

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       Lau y Manny
 
-    <Timeline>
+    <Timeline mode="alternate">
       <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
       <Timeline.Item>Solve initial network problems 2015-09-01</Timeline.Item>
       <Timeline.Item>Technical testing 2015-09-01</Timeline.Item>
