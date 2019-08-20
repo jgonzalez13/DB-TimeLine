150bd14f58044584c1587c49b0e4f877d3f46438
Emmanuel Gonzalez <jgonzalez13@ucol.mx>
2019-08-19 21:52:03 -0500

    header y footer

--- src/components/Footer.component.jsx
+++ src/components/Footer.component.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
 const Footer = () => ( 
-    <footer>soy el footer</footer>
+    <footer>
+        <div>
+            <span><strong>Fecaha de entrega: martes, 20 de agosto de 2019, 00:00</strong> </span>
+        </div>
+        <span>Integrantes:</span>
+        <ul>
+            <li>Norman Lau Heredia</li>
+            <li>Jesus Emmanuel Gonzalez Vega</li>
+        </ul>
+    </footer>
 )
 
 export default Footer
--- src/components/Header.component.jsx
+++ src/components/Header.component.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const Header = () => (
-    <div>Aqui va el titulo</div>
+    <div className="header">
+        <h1 className="header-title">
+            <span>Línea del tiempo</span>
+            <span>Evolución tecnológica</span>
+        </h1>
+    </div>
 )
 
 export default Header 
